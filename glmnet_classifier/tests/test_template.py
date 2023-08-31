8604c6f78a98ee5bfa44328177ed27eb989a02d5
rolf <hrolfrc@gmail.com>
2023-08-31 14:49:22 -0600

    test on seen and unseen data

--- glmnet_classifier/tests/test_template.py
+++ glmnet_classifier/tests/test_template.py
@@ -8,6 +8,8 @@ License: 3-clause BSD
 """
 import pytest
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 from glmnet_classifier import GlmnetClassifier
 
@@ -19,12 +21,32 @@ def data():
 
 def test_GlmnetClassifier(data):
     X, y = data
-    clf = GlmnetClassifier()
+    clf = GlmnetClassifier().fit(X, y)
 
-    clf.fit(X, y)
     assert hasattr(clf, 'classes_')
     assert hasattr(clf, 'X_')
     assert hasattr(clf, 'y_')
 
+    # =====================================
+    # How well glmnet learns the dataset
     y_pred = clf.predict(X)
-    assert y_pred.shape == (X.shape[0],)
+    assert len(y) == len(y_pred)
+
+    accuracy = accuracy_score(y_true=y, y_pred=y_pred)
+    assert round(accuracy, 4) == 0.9533
+
+    # =====================================
+    # how well glmnet learns unseen data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        random_state=42
+    )
+
+    clf = GlmnetClassifier().fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    assert len(y_test) == len(y_pred)
+
+    accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+    assert round(accuracy, 4) == 0.9737
+
+
